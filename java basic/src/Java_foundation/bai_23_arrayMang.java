37d7a2aa02354ecddce052e215c6c0810cb50453
Kelvin Huynh <102346766+KelvinPhu@users.noreply.github.com>
2023-11-17 11:17:07 +0700

    Update bai_23_arrayMang.java

--- java basic/src/Java_foundation/bai_23_arrayMang.java
+++ java basic/src/Java_foundation/bai_23_arrayMang.java
@@ -18,8 +18,6 @@ public class bai_23_arrayMang {
 		System.out.println("total: " +total);
 		
 		
-		
-		
 		double array2[] = new double[] {32,43,54,235,3453,5345.435,45,3453,34534,53};
 		for (int i = 0; i < array2.length; i++) {
 			System.out.println("print out array 2: "+array2[i]);
